249edcdc53a01c2c742d8187c8b75a06b5425e70
Miura Manato <mmiura2351@gmail.com>
2024-11-01 15:38:10 +0000

    log追加

--- src/bot_class.py
+++ src/bot_class.py
@@ -1,20 +1,37 @@
+"""This module contains the MyBot class for interacting with Discord."""
+
 import discord
 from discord import app_commands
 
+from utils.logger import Logger
+
 
 class MyBot(discord.Client):
-    def __init__(self, *, intents: discord.Intents, guild_id: str | None = None):
+    """A Discord bot client for handling interactions and commands."""
+
+    def __init__(
+        self,
+        *,
+        intents: discord.Intents,
+        guild_id: str | None = None,
+    ) -> None:
+        """Initialize MyBot with specified intents and optional guild ID.
+
+        Args:
+            intents (discord.Intents): The intents for the bot.
+            guild_id (str | None, optional): The ID of the guild. Defaults to None.
+        """
         super().__init__(intents=intents)
+        self.logger = Logger(logfile="logs/bot.log", name="botLogger", level=10)
         self.guild_id = guild_id
         self.tree = app_commands.CommandTree(self)
 
-    async def setup_hook(self):
-        # sync command.
+    async def setup_hook(self) -> None:
+        """Synchronize commands with the specified guild."""
         guild = discord.Object(id=self.guild_id)
         self.tree.copy_global_to(guild=guild)
-        res = await self.tree.sync(guild=guild)
-        print(res)
+        await self.tree.sync(guild=guild)
 
-    async def on_ready(self):
-        print(f"Logged in as {self.user} (ID: {self.user.id})")
-        print("------")
+    async def on_ready(self) -> None:
+        """Handle the event when the bot is ready."""
+        self.logger.info(f"Logged in as {self.user} (ID: {self.user.id})")  # noqa: G004
